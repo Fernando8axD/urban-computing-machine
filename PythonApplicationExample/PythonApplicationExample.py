7dcc5c86aec7607984a9fedd517ee53583e8504f
Fernando Ochoa <56469077+ferochoavi@users.noreply.github.com>
2020-04-09 04:20:06 -0500

    Added a new code
    I added a new app which I crated in VS Community 2019 and inside the app code that determines if a number ir pair or not pair.

---
+++ PythonApplicationExample/PythonApplicationExample.py
@@ -0,0 +1,7 @@
+for i in range(9):
+    if i % 2 == 0:
+        print("este bello numero es: {} y es par".format(i))
+    else:
+        print("este horrendo numero es: {} y es regular".format(i))
+
+
