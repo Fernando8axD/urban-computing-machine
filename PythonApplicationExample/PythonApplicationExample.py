a4b9de72fce05af26192627aa8da0f90a590e0a4
Fernando Ochoa <56469077+ferochoavi@users.noreply.github.com>
2020-04-09 04:36:05 -0500

    I broke the code
    It's almost 4 am and idk what the heck I did

--- PythonApplicationExample/PythonApplicationExample.py
+++ PythonApplicationExample/PythonApplicationExample.py
@@ -4,4 +4,19 @@ for i in range(9):
     else:
         print("este horrendo numero es: {} y es regular".format(i))
 
+print("este es un cambio para probar github desktop\n")
+
+
+
+print("Imprimiremos los numeros primos debajo de un numero que se le pida al usuario\n")
+
+num = int(input("Profi escribe un numero, rey...\n"))
+
+for number in range(num):
+    #acu = 0
+    print(number+1)
+    for scheisse in range(number):
+        print(number+1)
+        
+
 
